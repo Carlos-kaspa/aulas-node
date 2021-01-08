60aaa3a552012bf47a57cf12262b5bfd42748e00
Carlos-kaspa <carloslimatwg@gmail.com>
2021-01-08 11:10:27 -0300

    validação de tipo de arquivo para imagem

--- arquivos/fileUpload.js
+++ arquivos/fileUpload.js
@@ -1,3 +1,4 @@
+const { throws } = require('assert');
 const fs = require('fs');
 
 
@@ -6,12 +7,25 @@ module.exports = (path,filename,callback) => {
     const fileName = path.split('.') //separa os elementos do path criando um array
     const fileExtension = fileName.slice(-1) //o ultimo índice do array é a extensão o arquivo
     // const fileExtension = path.extname(path) path.extname também busca qual é a extensão do arquivo
-    
     const newPath = `./assets/imagens/${filename}.${fileExtension}`
+  
+    switch(fileExtension[0]){
+        case 'jpg':
+        case 'jpeg':
+        case 'png':
+        case 'bitmap':
+            dataStream(path,newPath)
+            break;
 
-    console.log(newPath)
+        default:
+            throw "formato de arquivo inválido"   
+    }
 
-    fs.createReadStream(path)
-    .pipe(fs.createWriteStream(newPath))
-    .on('finish',() => callback(newPath))
+    
+
+    function dataStream(path,newPath){
+        fs.createReadStream(path)
+        .pipe(fs.createWriteStream(newPath))
+        .on('finish',() => callback(newPath))
+    }
 }
